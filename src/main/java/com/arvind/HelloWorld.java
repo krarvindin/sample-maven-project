36209c084ab7eda5633a06c1bb30f6689fdd6100
Arvind Krishnan <Akrishnan37@ibahealth.local>
2020-04-10 12:57:34 +0530

    First commit to remote repo

---
+++ src/main/java/com/arvind/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.arvind;
+
+/**
+ * @author Akrishnan37
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
